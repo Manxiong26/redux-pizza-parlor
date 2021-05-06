be7d791c1cd11f7fbd4ea32d46f35e2f728aa0b1
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-06 12:55:32 -0500

    added GET route

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -6,7 +6,24 @@ function PizzaList() {
 
     const dispatch = useDispatch();
 
-    function setPizzaList() {
-        
+    function getPizzaList() {
+
+        axios({
+            method: 'GET',
+            url: '/api/pizza',
+        })
+            .then((response) => {
+                console.log('Getting pizzas from Database!', response);
+                dispatch({ type: 'SET_PIZZA_LIST', payload: response.data })
+            })
+            .catch(error => {
+                console.log('Error getting pizzas from Database.', error);
+                alert('Error with GET');
+            })
     }
+
+    useEffect(() => {
+        console.log('In useEffect');
+        getPizzaList();
+    }, []);
 }
