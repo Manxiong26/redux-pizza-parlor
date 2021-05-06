ab4d232cf3f359d93da982277ab001ed32dae765
Robyn Noble <robynhurd09@gmail.com>
2021-05-06 13:04:33 -0500

    admin orders template created

---
+++ src/components/AdminOrders/AdminOrders.jsx
@@ -0,0 +1,17 @@
+function AdminOrders(){
+
+    return(
+        <section>
+            <h1>Prime Pizza Orders</h1>
+            <table>
+                <tr>
+                    <td>Name</td>
+                    <td>Time Order Was Placed</td>
+                    <td>Type</td>
+                    <td>Total Cost</td>
+                </tr>
+                <tr></tr>
+            </table>
+        </section>
+    )
+}
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import CustomerForm from '../CustomerForm/CustomerForm';
+import AdminOrders from '../AdminOrders/AdminOrders';
 
 function App() {
 
