47121e9b6a7f32b64789065d1111bda7788e64a3
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-06 13:59:28 -0500

    attempting to push to master

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
 import CustomerForm from '../CustomerForm/CustomerForm';
 import AdminOrders from '../AdminOrders/AdminOrders';
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+
 ReactDOM.render(<App />, document.getElementById('root'));
