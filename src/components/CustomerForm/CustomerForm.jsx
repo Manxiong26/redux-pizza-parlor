f0e31e13a6cd75694f95eea44d96207cba361200
Robyn Noble <robynhurd09@gmail.com>
2021-05-06 11:52:23 -0500

    history and checkout total added

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,6 +1,10 @@
 import {useHistory} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 function CustomerForm (){
+
+    const reduxStore = useSelector(store => store);
+    const history = useHistory();
 
     function handleSubmit(event){
         event.PreventDefault();
@@ -8,29 +12,35 @@ function CustomerForm (){
         console.log('inside handleSubmit on CustomerForm');
     }
     return(
-        <form>
-            <input
-                required
-                placeholder='Name'
-                value={}
-            />
-            <input
-                required
-                placeholder='Street Address'
-                value={}
-            />
-            <input
-                required
-                placeholder='City'
-                value={}
-            />
-            <input
-                required
-                placeholder='Zipcode'
-                value={}
-            />
-            <button onClick={handleSubmit}>Next</button>
-        </form>
+        <div>
+            <h1>Prime Pizza</h1>
+            <p>Total {reduxStore.price}</p>
+            <br />
+            <h2>Step: 2 Customer Information</h2>
+            <form>
+                <input
+                    required
+                    placeholder='Name'
+                    value={}
+                />
+                <input
+                    required
+                    placeholder='Street Address'
+                    value={}
+                />
+                <input
+                    required
+                    placeholder='City'
+                    value={}
+                />
+                <input
+                    required
+                    placeholder='Zipcode'
+                    value={}
+                />
+                <button onClick={handleSubmit}>Next</button>
+            </form>
+        </div>
     )
 }
 export default CustomerForm;
