c5cb174e3f13772b33c35643cda9644b5877f0af
Maneena Xiong <xiong.maneena@gmail.com>
2021-05-06 12:59:47 -0500

    create the get and post

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,12 +9,33 @@ function Home(){
     useEffect(() => {
         console.log('in useEffect')
         getStudents();
+        getOder();
     }, [])
 
 const getPizzaList = () => {
     axios.get('/api/pizza')
     .then((response) => {
         dispatch({type: 'SET_PIZZA_LIST', payload: response.data})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+const getOder = () =>{
+    axios.get('/api/order')
+    .then((response) => {
+        dispatch({type: 'SET_ORDER', payload: response.data})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+const addPizza = () => {
+    axios.post('//api/pizza')
+    .then((response) => {
+        console.log(response);
+        getPizzaList();
     })
     .catch((err) => {
         console.log(err);
