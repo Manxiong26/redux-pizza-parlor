03b3f7176f6cb62c9810157e2f9c06f892851136
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-06 12:43:10 -0500

    added & working on PizzaList component

---
+++ src/components/PizzaList/PizzaList.jsx
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function PizzaList() {
+
+    const dispatch = useDispatch();
+
+    function setPizzaList() {
+        
+    }
+}
